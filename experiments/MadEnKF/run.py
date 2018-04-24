5dd30bb86fa233f5460fceb233e0b3a13d0e5317
Alexander Gruber <reddy.gruber@icloud.com>
2018-04-24 16:21:07 +0200

    parameter changes and timer integrated

--- experiments/MadEnKF/run.py
+++ experiments/MadEnKF/run.py
@@ -15,6 +15,7 @@ from myprojects.readers.mswep import MSWEP_io
 
 from myprojects.timeseries import calc_anomaly
 
+from timeit import default_timer
 
 def main():
 
@@ -25,27 +26,29 @@ def main():
     p = Pool(24)
     p.map(run, cells)
 
-    #cells = 601
-    #for cell in np.atleast_1d(cells):
+    # cells = 601
+    # for cell in np.atleast_1d(cells):
     #    run(cell)
 
 def run(cell):
 
+    t = default_timer()
+
+    precip = mswep.read(info.name)
     ascat = HSAF_io()
     mswep = MSWEP_io()
 
     if platform.system() == 'Windows':
-        result_file = os.path.join('D:', 'work', 'API', 'AdEnKF', 'result_%04i.csv' % cell)
+        result_file = os.path.join('D:', 'work', 'MadEnKF', 'API', 'CONUS', 'result_%04i.csv' % cell)
     else:
         result_file = os.path.join('/', 'scratch', 'leuven', '320', 'vsc32046', 'output', 'MadEnKF', 'API', 'result_%04i.csv' % cell)
 
     dt = ['2012-01-01','2016-12-31']
 
     for data, info in mswep.iter_cell(cell):
-	
-	#if True:
+
+        #if True:
         try:
-            precip = mswep.read(info.name)
             sm = ascat.read(info.dgg_gpi)
 
             if (precip is None) | (sm is None):
@@ -54,12 +57,13 @@ def run(cell):
             precip = calc_anomaly(precip[dt[0]:dt[1]], method='harmonic', longterm=False)
             sm = calc_anomaly(sm[dt[0]:dt[1]], method='harmonic', longterm=False)
 
+
             df = pd.DataFrame({1: precip, 2: sm}, index=pd.date_range(dt[0],dt[1]))
             df.loc[np.isnan(df[1]), 1] = 0.
 
             api = API(gamma=info.gamma)
 
-            x_ana, P_ana, R, Q, H, checkvar = MadEnKF(api, df[1].values, df[2].values, n_ens=42, n_iter=17)
+            x_ana, P_ana, R, Q, H, checkvar = MadEnKF(api, df[1].values, df[2].values, n_ens=42, n_iter=13)
 
             result = pd.DataFrame({'lon': info.lon, 'lat': info.lat,
                                    'col': info.col, 'row': info.row,
@@ -73,6 +77,8 @@ def run(cell):
         except:
             print 'GPI failed.'
             continue
+
+    print '%i finished in %i hr' % (cell, default_timer() - t)
 
     ascat.close()
     mswep.close()
