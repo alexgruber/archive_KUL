43c07172eba53533123b3bc3c3efed0c4fd3c3f5
Alexander Gruber <reddy.gruber@icloud.com>
2020-10-16 17:15:16 +0200

    updated MadKF validation for matching time periods

--- experiments/MadKF/CLSM/validate_madkf_smap.py
+++ experiments/MadKF/CLSM/validate_madkf_smap.py
@@ -7,6 +7,7 @@ warnings.filterwarnings("ignore")
 import numpy as np
 import pandas as pd
 
+from math import floor
 from pathlib import Path
 from itertools import repeat
 from multiprocessing import Pool
@@ -29,6 +30,10 @@ from validation_good_practice.plots import plot_ease_img
 
 def run_ascat_eval(iteration, n_procs=1):
 
+    res_path = Path(f'~/Documents/work/MadKF/CLSM/SMOS40/validation/iter_{iteration}').expanduser()
+    if not res_path.exists():
+        Path.mkdir(res_path, parents=True)
+
     it = repeat(iteration, n_procs)
     part = np.arange(n_procs) + 1
     parts = repeat(n_procs, n_procs)
@@ -36,7 +41,6 @@ def run_ascat_eval(iteration, n_procs=1):
     p = Pool(n_procs)
     p.starmap(run_ascat_eval_part, zip(it, part, parts))
 
-    res_path = Path(f'~/Documents/work/MadKF/CLSM/SMAP/validation/iter_{iteration}').expanduser()
     merge_files(res_path, pattern='ascat_eval_part*.csv', fname='ascat_eval.csv', delete=True)
 
 def run_ascat_eval_part(iteration, part, parts, ref='ascat'):
@@ -48,7 +52,7 @@ def run_ascat_eval_part(iteration, part, parts, ref='ascat'):
         stg = None
         smap_path = Path('/Users/u0116961/data_sets/SMAP/timeseries')
 
-    res_path = Path(f'~/Documents/work/MadKF/CLSM/SMAP/validation/iter_{iteration}').expanduser()
+    res_path = Path(f'~/Documents/work/MadKF/CLSM/SMOS40/validation/iter_{iteration}').expanduser()
     if not res_path.exists():
         Path.mkdir(res_path, parents=True)
 
@@ -65,16 +69,26 @@ def run_ascat_eval_part(iteration, part, parts, ref='ascat'):
     # Look-up table that contains the grid cells to iterate over
     lut = lut.iloc[start:end, :]
 
-    names = ['open_loop', 'SMOSSMAP_short', 'SMOS40_it631'] + [f'SMAP_it{iteration}{i}' for i in range(1,4)]
-    runs = ['US_M36_SMAP_TB_OL_noScl', 'US_M36_SMAP_TB_DA_scl_SMOSSMAP_short', 'US_M36_SMOS40_TB_MadKF_DA_it613'] + [f'US_M36_SMAP_TB_MadKF_DA_it{iteration}{i}' for i in range(1,4)]
-    roots = [stg, stg, stg, None, None, None]
+    # n_subits = 3
+    # names = ['open_loop', 'SMOSSMAP_short', 'SMOS40_it631'] + [f'SMAP_it{iteration}{i}' for i in range(1,n_subits + 1)]
+    # runs = ['US_M36_SMAP_TB_OL_noScl', 'US_M36_SMAP_TB_DA_scl_SMOSSMAP_short', 'US_M36_SMOS40_TB_MadKF_DA_it613'] + [f'US_M36_SMAP_TB_MadKF_DA_it{iteration}{i}' for i in range(1,n_subits + 1)]
+    # roots = [stg, stg, stg] + [None for i in range(1,n_subits + 1)]
+
+    names = ['open_loop', 'SMOSSMAP_short'] + \
+            [f'SMAP_it3{i}' for i in range(1,5)] + \
+            [f'SMOS40_it61{i}' for i in range(3,6)]
+    runs = ['US_M36_SMAP_TB_OL_noScl', 'US_M36_SMAP_TB_DA_scl_SMOSSMAP_short'] + \
+           [f'US_M36_SMAP_TB_MadKF_DA_it3{i}' for i in  range(1,5)] + \
+           [f'US_M36_SMOS40_TB_MadKF_DA_it61{i}' for i in range(3,6)]
+    roots = [None for i in range(len(runs))]
 
     dss = [LDAS_io('xhourly', run, root=root).timeseries for run, root in zip(runs, roots)]
-    grid = LDAS_io('xhourly', runs[0], root=stg).grid
+    grid = LDAS_io('ObsFcstAna', runs[0], root=stg).grid
 
     # t_ana = pd.DatetimeIndex(LDAS_io('ObsFcstAna', runs[0]).timeseries.time.values).sort_values()
     ds_obs_smap = (LDAS_io('ObsFcstAna', 'US_M36_SMAP_TB_OL_noScl', root=stg).timeseries['obs_ana'])
-    ds_obs_smos = (LDAS_io('ObsFcstAna', 'US_M36_SMOS40_TB_MadKF_DA_it613', root=stg).timeseries['obs_ana'])
+    ds_obs_smos1 = (LDAS_io('ObsFcstAna', 'US_M36_SMOS40_TB_MadKF_DA_it613', root=stg).timeseries['obs_ana'])
+    ds_obs_smos2 = (LDAS_io('ObsFcstAna', 'US_M36_SMOS40_TB_MadKF_DA_it615', root=stg).timeseries['obs_ana'])
 
     modes = ['absolute', 'longterm', 'shortterm']
 
@@ -106,10 +120,18 @@ def run_ascat_eval_part(iteration, part, parts, ref='ascat'):
             except:
                 continue
 
+
+        t_df_smos1 = ds_obs_smos1.sel(species=[1, 2]).isel(lat=row, lon=col).to_pandas()
+        t_df_smos2 = ds_obs_smos2.sel(species=[1, 2]).isel(lat=row, lon=col).to_pandas()
+        t_ana_smos1 = t_df_smos1[~np.isnan(t_df_smos1[1]) | ~np.isnan(t_df_smos1[2])].resample('1d').mean().index
+        t_ana_smos2 = t_df_smos2[~np.isnan(t_df_smos2[1]) | ~np.isnan(t_df_smos2[2])].resample('1d').mean().index
+
+        t_ana_smos_u = t_ana_smos1.union(t_ana_smos2)
+        t_ana_smos_i = t_ana_smos1.intersection(t_ana_smos2)
+
         t_df_smap = ds_obs_smap.sel(species=[1, 2]).isel(lat=row, lon=col).to_pandas()
-        t_df_smos = ds_obs_smos.sel(species=[1, 2]).isel(lat=row, lon=col).to_pandas()
-        t_ana_smap = t_df_smap[~np.isnan(t_df_smap[1]) | ~np.isnan(t_df_smap[2])].resample('1d').mean().index
-        t_ana_smos = t_df_smos[~np.isnan(t_df_smos[1]) | ~np.isnan(t_df_smos[2])].resample('1d').mean().index
+        t_ana_smap_u = t_df_smap[~np.isnan(t_df_smap[1]) | ~np.isnan(t_df_smap[2])].resample('1d').mean().index
+        t_ana_smap_i = t_ana_smap_u[(t_ana_smap_u >= t_ana_smos_i.min()) & (t_ana_smap_u <= t_ana_smos_i.max())]
 
         var = 'sm_surface'
         for mode in modes:
@@ -127,7 +149,10 @@ def run_ascat_eval_part(iteration, part, parts, ref='ascat'):
 
             for run, ts_model in zip(names, dss):
 
-                t_ana = t_ana_smos if run == 'SMOS40_it631' else t_ana_smap
+                if 'SMOS40' in run:
+                    t_ana_u, t_ana_i = t_ana_smos_u, t_ana_smos_i
+                else:
+                    t_ana_u, t_ana_i = t_ana_smap_u, t_ana_smap_i
 
                 ind = (ts_model['snow_mass'][:, row, col].values == 0) & (
                         ts_model['soil_temp_layer1'][:, row, col].values > 277.15)
@@ -145,10 +170,15 @@ def run_ascat_eval_part(iteration, part, parts, ref='ascat'):
                 r, p = pearsonr(tmp[1], tmp[2]) if len(tmp) > 10 else (np.nan, np.nan)
                 res['r_' + run + '_' + mode] = r
 
-                tmp = pd.DataFrame({1: ts_ref, 2: ts_mod}).reindex(t_ana).dropna()
-                res['ana_len_' + run + '_' + mode] = len(tmp)
+                tmp = pd.DataFrame({1: ts_ref, 2: ts_mod}).reindex(t_ana_u).dropna()
+                res['ana_u_len_' + run + '_' + mode] = len(tmp)
                 r, p = pearsonr(tmp[1], tmp[2]) if len(tmp) > 10 else (np.nan, np.nan)
-                res['ana_r_' + run + '_' + mode] = r
+                res['ana_u_r_' + run + '_' + mode] = r
+
+                tmp = pd.DataFrame({1: ts_ref, 2: ts_mod}).reindex(t_ana_i).dropna()
+                res['ana_i_len_' + run + '_' + mode] = len(tmp)
+                r, p = pearsonr(tmp[1], tmp[2]) if len(tmp) > 10 else (np.nan, np.nan)
+                res['ana_i_r_' + run + '_' + mode] = r
 
         if not result_file.exists():
             res.to_csv(result_file, float_format='%0.3f')
@@ -157,28 +187,33 @@ def run_ascat_eval_part(iteration, part, parts, ref='ascat'):
 
 def plot_eval(iteration):
 
-    analysis_only = True
+    analysis_only = False
     relative = True
-    ref_exp = 'SMOS40_it631'
+    ref_exp = 'open_loop'
 
     sub = 'ana_' if analysis_only else ''
     fext = '_rel' if relative else '_abs'
 
-    res = pd.read_csv(Path(f'~/Documents/work/MadKF/CLSM/SMAP/validation/iter_{iteration}/ascat_eval.csv').expanduser(), index_col=0)
+    # res = pd.read_csv(Path(f'~/Documents/work/MadKF/CLSM/SMAP/validation/iter_{iteration}/ascat_eval.csv').expanduser(), index_col=0)
+    res1 = pd.read_csv(Path(f'~/Documents/work/MadKF/CLSM/SMAP/validation/iter_3/ascat_eval.csv').expanduser(), index_col=0)
+    res2 = pd.read_csv(Path(f'~/Documents/work/MadKF/CLSM/SMOS40/validation/iter_6/ascat_eval.csv').expanduser(), index_col=0)
     fbase = Path(f'~/Documents/work/MadKF/CLSM/SMAP/plots/iter_{iteration}').expanduser()
     if not fbase.exists():
         Path.mkdir(fbase, parents=True)
 
     cbr_r = [0.6, 1.4] if relative else [0.2,0.7]
     cbr_r = [-0.3, 0.3] if relative else [0.2,0.7]
-    cbr_len = [500, 1500]
+    cbr_len = [2e2, 2e3]
 
-    exps = ['SMOS40_it631', 'SMOSSMAP_short'] + [f'SMAP_it{iteration}{i}' for i in range(1, 4)]
-    modes = ['absolute', 'longterm', 'shortterm']
+    n_subits = 4
+    # exps = ['SMOSSMAP_short'] + [f'SMAP_it{iteration}{i}' for i in range(1, n_subits + 1)] + ['SMOS40_it631']
+    exps = ['SMOSSMAP_short', 'SMAP_it34', 'SMOS40_it614', 'SMOS40_it615', 'SMOS40_it613']
+    # exps = [f'SMAP_it{iteration}{i}' for i in range(1, n_subits + 1)]
+    modes = ['absolute', 'shortterm', 'longterm']
 
-    # params = ['len', 'r']
-    # cbranges = [cbr_len, cbr_r]
-
+    # params = ['ana_len']
+    # cbranges = [cbr_len, cbr_len, cbr_r]
+    #
     params = ['r']
     cbranges = [cbr_r]
 
@@ -188,42 +223,64 @@ def plot_eval(iteration):
 
         cmap = 'seismic_r' if (p == 'r') & relative else 'YlGn'
 
-        f = plt.figure(figsize=(25,8))
+        f = plt.figure(figsize=(29,8))
 
         for i, m in enumerate(modes):
             for j, e in enumerate(exps):
+
+                if 'SMOS40' in e:
+                    p = 'ana_i_r'
+                else:
+                    p = 'r'
 
                 plt.subplot(len(modes),len(exps), len(exps)*i + j + 1)
 
                 col = f'{sub}{p}_{e}_{m}'
                 if relative and (p != 'len'):
-                    ref_col = f'{sub}{p}_{ref_exp}_{m}'
-                    res[f'{col}_diff'] = (res[col] - res[ref_col])  / (1 - res[ref_col])
+                    ref_col = f'{sub}r_{ref_exp}_{m}'
+                    if col in res1:
+                        res1[f'{col}_diff'] = (res1[col] - res1[ref_col]) / (1 - res1[ref_col])
+                    else:
+                        res1[f'{col}_diff'] = (res2[col] - res1[ref_col]) / (1 - res1[ref_col])
+
                     ext = '_diff'
                 else:
+                    if col in res2:
+                        res1[col] = res2[col]
                     ext = ''
 
-                im = plot_ease_img(res, f'{col}{ext}' , title=f'{e} / {m}', cmap=cmap, cbrange=cbr, fontsize=fontsize, print_median=True)
+                log_scale = True if 'len' in p else False
+                im = plot_ease_img(res1, f'{col}{ext}' , title=f'{e} / {m}', cmap=cmap, cbrange=cbr, fontsize=fontsize, print_median=True, log_scale=log_scale)
                 # if (i == 2) & (j == 1):
                 #     ax = im.axes
 
-        f.subplots_adjust(wspace=0.04, hspace=0.025, bottom=0.06)
-        # pos1 = f.axes[-3].get_position()
-        # pos2 = f.axes[-4].get_position()
-        # x1 = (pos1.x0 + pos1.x1) / 2
-        # x2 = (pos2.x0 + pos2.x1) / 2
-        pos = f.axes[-3].get_position()
-        x1 = pos.x0
-        x2 = pos.x1
-        cbar_ax = f.add_axes([x1, 0.03, x2-x1, 0.03])
-        cbar = f.colorbar(im, orientation='horizontal', cax=cbar_ax)
-        for t in cbar.ax.get_xticklabels():
-            t.set_fontsize(fontsize)
+        plot_centered_cbar(f, im, len(exps), fontsize=fontsize)
 
         f.savefig(fbase / f'ascat_eval_{sub}{p}{fext}.png', dpi=300, bbox_inches='tight')
         plt.close()
 
         # plt.show()
+
+def plot_centered_cbar(f, im, n_cols, wspace=0.04, hspace=0.025, bottom=0.06, fontsize=12):
+
+    f.subplots_adjust(wspace=wspace, hspace=hspace, bottom=bottom)
+
+    ctr = n_cols/2 * -1
+    if ctr % 1 == 0:
+        pos1 = f.axes[int(ctr) - 1].get_position()
+        pos2 = f.axes[int(ctr)].get_position()
+        x1 = (pos1.x0 + pos1.x1) / 2
+        x2 = (pos2.x0 + pos2.x1) / 2
+    else:
+        pos = f.axes[floor(ctr)].get_position()
+        x1 = pos.x0
+        x2 = pos.x1
+
+    cbar_ax = f.add_axes([x1, 0.03, x2 - x1, 0.03])
+    cbar = f.colorbar(im, orientation='horizontal', cax=cbar_ax)
+    for t in cbar.ax.get_xticklabels():
+        t.set_fontsize(fontsize)
+
 
 def plot_ascat_smap_eval(iteration):
 
@@ -302,12 +359,12 @@ def plot_ascat_smap_eval(iteration):
 
 if __name__=='__main__':
 
-    iteration = 2
+    iteration = 6
 
-    run_ascat_eval(iteration, n_procs=9)
-    # run_ascat_eval_part(iteration, 2, 2)
+    # run_ascat_eval(iteration, n_procs=14)
+    run_ascat_eval_part(iteration, 2, 2)
 
-    plot_eval(iteration)
+    # plot_eval(iteration)
     # plot_ascat_smap_eval(iteration)
 
 '''
